d1ec782711fd333506715d6ed6016d8527e7f41a
OtiliaNicola <otilianicola94@gmail.com>
2025-01-21 09:51:09 +0100

    fix: change order view

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,41 +1,20 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { ExperiencesComponent } from "../experience/experience.component";
-import { SkillsComponent } from "../skills/skills.component";
-import { ProjectsComponent } from "../projects/projects.component";
-import { EducationsComponent } from "../educations/educations.component";
-import { MyWorksComponent } from "../my-works/my-works.component";
-import { ContactComponent } from "../contact/contact.component";
-import { PracticeComponent } from "../practice/practice.component";
-import { HeroCardComponent } from "../../shared/hero-card/hero-card.component";
-
-
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   imports: [
-    RouterOutlet,
-    ExperiencesComponent,
-    SkillsComponent,
-    ProjectsComponent,
-    EducationsComponent,
-    MyWorksComponent,
-    ContactComponent,
-    PracticeComponent,
-    HeroCardComponent
-  ],
-  templateUrl: './home-page.component.html'
+    CommonModule,
+    
+],
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
+  @Input() coderName: string = '';
+  @Input() title: string = '';
   isNavOpen = false;
-  skills = [
-    'Angular', 'React', 'Ionic',
-    'HTML5', 'CSS3', 'SCSS',
-    'TypeScript', 'JavaScript',
-    'Bootstrap', 'Node JS',
-    'Express JS', 'SQL', 'Git'
-  ];
 
   toggleNav() {
     this.isNavOpen = !this.isNavOpen;
