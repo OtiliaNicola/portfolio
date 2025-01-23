2bebbed49903e39c2685fec5c0a108182943f2ea
OtiliaNicola <otilianicola94@gmail.com>
2025-01-23 13:08:49 +0100

    Fix; add atropos js efects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,90 +1,71 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectCardComponent } from '../../shared/project-card/project-card.component';
-
-
-interface Project {
-  title: string;
-  description: string;
-  badgeColors: string[];
-  tools: string[];
-  myRole: string;
-  githubUrl: string;
-  demoUrl: string;
-  topPosition?: string; // Añadido como opcional
-}
-
-interface Education {
-  title: string;
-  institution: string;
-  period: string;
-  details?: string[];
-}
+import { Project } from '../../core/interfaces/project.interface';
+import Atropos from 'atropos';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [CommonModule, ProjectCardComponent],
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent {
-  projects = [
+export class ProjectsComponent implements AfterViewInit {
+  projects: Project[] = [
     {
-      title: 'Portfolio',
-      description: 'Portafolio personal desarrollado con Angular 19, donde muestro mis proyectos y habilidades como desarrolladora Frontend.',
+      title: ' Calculator',
+      description:
+        'This GitHub repository contains a calculator made with HTML, CSS and JavaScript.',
+      badgeColors: ['#FFA500', '#FFD700'],
+      tools: ['JavaScript', 'HTML', 'SCSS'],
+      myRole: 'Frontend Developer',
+      githubUrl: 'https://github.com/OtiliaNicola/calculator',
+      demoUrl: 'https://otilianicola.github.io/calculator/',
+    },
+    {
+      title: 'TaskNest',
+      description:
+        'TaskNest is a task management web application developed with Angular.',
+      badgeColors: ['#61DAFB', '#282C34'],
+      tools: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'Angular',
+        'TypeScript',
+        'Bootstrap',
+      ],
+      myRole: 'Frontend Developer',
+      githubUrl: 'https://github.com/OtiliaNicola/task-nest',
+      demoUrl: 'https://task-nest-three.vercel.app',
+    },
+    {
+      title: 'Angular Gifs App',
+      description: 'Gifs application from my Angular course',
       badgeColors: ['#00DC82', '#41B883'],
       tools: ['Angular', 'TypeScript', 'SCSS', 'Bootstrap'],
       myRole: 'Frontend Developer',
-      githubUrl: 'https://github.com/OtiliaNicola/portfolio',
-      demoUrl: ''
+      githubUrl: 'https://github.com/OtiliaNicola/angular-gif-app',
+      demoUrl: 'https://myfirst-angular-gif.netlify.app',
     },
-    {
-      title: 'IOS Calculator',
-      description: 'Calculadora inspirada en iOS, desarrollada con JavaScript vanilla, HTML y CSS.',
-      badgeColors: ['#FFA500', '#FFD700'],
-      tools: ['JavaScript', 'HTML', 'CSS'],
-      myRole: 'Frontend Developer',
-      githubUrl: 'https://github.com/OtiliaNicola/ios_calculator',
-      demoUrl: 'https://ios-calculator-gamma.vercel.app/'
-    },
-    {
-      title: 'Space Tourism',
-      description: 'Proyecto Frontend Mentor de turismo espacial, desarrollado con React.',
-      badgeColors: ['#61DAFB', '#282C34'],
-      tools: ['React', 'JavaScript', 'CSS'],
-      myRole: 'Frontend Developer',
-      githubUrl: 'https://github.com/OtiliaNicola/space-tourism',
-      demoUrl: 'https://space-tourism-sepia-mu.vercel.app/'
-    }
   ];
 
-  education: Education[] = [
-    {
-      title: 'Curso Fullstack Development Bootcamp',
-      institution: 'Adalab',
-      period: 'Marzo - Junio 2024',
-      details: [
-        'Desarrollo de aplicaciones web responsive usando HTML, CSS, JavaScript y React.',
-        'Backend con Node.js, Express y MySQL.',
-        'Control de versiones con Git.',
-        'Proyectos colaborativos utilizando filosofía Agile y Scrum.'
-      ]
-    },
-    {
-      title: 'Curso Angular de Cero a Experto',
-      institution: 'Udemy',
-      period: 'Agosto 2024',
-      details: [
-        'Fundamentos y arquitecturas de Angular',
-        'Creación de componentes y servicios',
-        'Manejo de formularios y directivas'
-      ]
-    },
-    {
-      title: 'FP Superior de Administración y Finanzas',
-      institution: 'Centro Cultural Salmantino',
-      period: 'Madrid (2021 - 2023)'
-    }
-  ];
+  ngAfterViewInit() {
+    Atropos({
+      el: '.myAtropos_0',
+      activeOffset: 40,
+      shadowScale: 1.05,
+    });
+    Atropos({
+      el: '.myAtropos_1',
+      activeOffset: 40,
+      shadowScale: 1.05,
+    });
+    Atropos({
+      el: '.myAtropos_2',
+      activeOffset: 40,
+      shadowScale: 1.05,
+    });
+  }
 }
