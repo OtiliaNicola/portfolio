f82d5be4bdaaea082055f3b74561d6b4aa4449c6
OtiliaNicola <otilianicola94@gmail.com>
2025-01-23 13:09:09 +0100

    fix: other changes

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,20 +1,22 @@
-import { CommonModule } from "@angular/common";
-import { Component, Input } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import JSConfetti from 'js-confetti';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [
-    CommonModule,
-    
-],
+  imports: [CommonModule],
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
-  @Input() coderName: string = '';
-  @Input() title: string = '';
+  title = input<string>();
   isNavOpen = false;
+  private jsConfetti: JSConfetti;
+
+  constructor() {
+    this.jsConfetti = new JSConfetti();
+  }
 
   toggleNav() {
     this.isNavOpen = !this.isNavOpen;
@@ -24,10 +26,20 @@ export class HomePageComponent {
     this.isNavOpen = false;
   }
 
-  downloadResume() {
+  async downloadCv() {
     const link = document.createElement('a');
     link.href = 'assets/OtiliaNicolaResume.pdf';
     link.download = 'OtiliaNicolaResume.pdf';
+    try {
+      // Add your resume download logic here
+      await this.jsConfetti.addConfetti({
+        emojis: ['🌟', '✨', '💫', '🎉'],
+        emojiSize: 70,
+        confettiNumber: 50,
+      });
+    } catch (error) {
+      console.error('Error triggering confetti:', error);
+    }
     link.click();
   }
 }
