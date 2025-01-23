29b88f310e18b553e02835cc55fb7bd400406d9f
OtiliaNicola <otilianicola94@gmail.com>
2025-01-23 13:06:24 +0100

    feat: instal transloco library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,30 @@ import { ProjectsComponent } from './components/projects/projects.component';
 import { ResourcesComponent } from './components/resources/resources.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
-
+import { TipsComponent } from './components/tips/tips.component';
+import { TranslocoDirective, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule, 
+    CommonModule,
     HomePageComponent,
     SkillsComponent,
     ProjectsComponent,
     ResourcesComponent,
-    ContactComponent
+    TipsComponent,
+    ContactComponent,
+    TranslocoDirective
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
+  constructor(private translocoService: TranslocoService) {}
+
+  switchLang(lang: string) {
+    this.translocoService.setActiveLang(lang);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideAnimations(),
-    provideToastr()
-  ]
+  providers: [provideRouter(routes), provideAnimations(), provideToastr(), provideHttpClient(), provideTransloco({
+        config: { 
+          availableLangs: ['en', 'es'],
+          defaultLang: 'en',
+          fallbackLang: 'en',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      })],
 };
